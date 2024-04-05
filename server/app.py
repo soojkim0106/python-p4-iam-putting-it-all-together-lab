221f5a8e7c4f3448f3da0b2ec90247c33db88863
Soojungkim <soojung.kimm@gmail.com>
2024-04-05 09:49:52 -0700

    confused with signup

--- server/app.py
+++ server/app.py
@@ -7,27 +7,63 @@ from sqlalchemy.exc import IntegrityError
 from config import app, db, api
 from models import User, Recipe
 
+
+@app.before_request
+def check_if_logged_in():
+    open_endpoint = ["signup", "login", "check_session"]
+    if request.endpoint not in open_endpoint and not session.get("user_id"):
+        return {"error": "Unauthorized"}, 401
+
+
 class Signup(Resource):
-    pass
+    def post(self):
+
+        data = request.get_json()
+        password = data.get("password")
+
+        user = User(
+            username=data.get("username"),
+            image_url=data.get("image_url"),
+            bio=data.get("bio"),
+        )
+
+        user.password_hash = password
+        try:
+            db.session.add(user)
+            # import ipdb; ipdb.set_trace()
+            User.query.delete()
+            db.session.commit()
+            
+            session["user_id"] = user.id
+
+            return user.to_dict(), 201
+
+        except Exception as e:
+            return {"message": str(e)}, 422
+
 
 class CheckSession(Resource):
     pass
 
+
 class Login(Resource):
     pass
+
 
 class Logout(Resource):
     pass
 
+
 class RecipeIndex(Resource):
     pass
 
-api.add_resource(Signup, '/signup', endpoint='signup')
-api.add_resource(CheckSession, '/check_session', endpoint='check_session')
-api.add_resource(Login, '/login', endpoint='login')
-api.add_resource(Logout, '/logout', endpoint='logout')
-api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
+
+api.add_resource(Signup, "/signup", endpoint="signup")
+api.add_resource(CheckSession, "/check_session", endpoint="check_session")
+api.add_resource(Login, "/login", endpoint="login")
+api.add_resource(Logout, "/logout", endpoint="logout")
+api.add_resource(RecipeIndex, "/recipes", endpoint="recipes")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,14 +1,60 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
 
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship("Recipe", back_populates = 'user')
+    
+    @hybrid_property
+    def password_hash(self):
+# sourcery skip: raise-specific-error
+        raise AttributeError("Password hashes may not be viewed.")
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8')
+        )
+        self._password_hash = password_hash.decode('utf-8')
+    
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8')
+        )
+    
+    def __repr__(self):
+        return f"User {self.username}, ID: {self.id}"
+
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
     
-    pass
+    __table_args__ = (db.CheckConstraint('length(instructions) > 50'), )
+    
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    user = db.relationship("User", back_populates="recipes")
+    
+    # @validates("instructions")
+    # def validate_instruction(self, _, text):
+    #     if not isinstance(text, str):
+    #         raise TypeError("Instructions must be string")
+    #     elif len(text) < 50:
+    #         raise ValueError("Instruction must be at least 50 characters")
+    #     return text
+    def __repr__(self):
+        return f'<Recipe {self.id} | {self.title}: {self.instructions}>'
